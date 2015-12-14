17751f342766c0f2bf35c934e10e4eab21a95358
Sergey Lupinos <sslupinos@gmail.com>
2015-12-14 23:09:03 +0300

    deploy settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :rvm_ruby_version, 'ruby-2.2.2@declarations'
 set :format,    :pretty
 set :log_level, :debug
 
-set :linked_files, %w{config/database.yml config/secrets.yml config/action_mailer.yml config/omniauth.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/uploads}
 
 set :keep_releases, 5
