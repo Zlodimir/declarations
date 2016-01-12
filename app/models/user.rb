6a0f60f2dc0894be3dc63b253e042ae69084dcec
Sergey Lupinos <sslupinos@gmail.com>
2016-01-12 20:59:14 +0300

    mailru auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,6 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :username, scope: :provider, case_sensitive: false
 
   def self.from_omniauth(auth, omniauth_params)
-    Rails.logger.debug("EBAT!!!!!!!!!!!#{auth}")
-    Rails.logger.debug("EBAT!!!!!!!!!provider!!#{auth.provider}")
-    Rails.logger.debug("EBAT!!!!!!!!!!!!!!!auth uid #{auth.uid}")
-    Rails.logger.debug("EBAT!!!!!!!!!!!!!!!auth_username(auth) #{auth_username(auth)}")
-    Rails.logger.debug("EBAT!!!!!!!!!!!!!!!auth_email(auth) #{auth_email(auth)}")
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth_email(auth)
       user.username = auth_username(auth)
@@ -20,22 +15,18 @@ class User < ActiveRecord::Base
     end
   end
 
-  # def email_required?
-  #   false
-  # end 
-
   private
 
   def self.auth_username(auth)
     case auth.provider
-    when 'facebook', 'twitter', 'google', 'vkontakte'
+    when 'facebook', 'twitter', 'google', 'vkontakte', 'mailru'
       auth.info.name
     end
   end
 
   def self.auth_email(auth)
     case auth.provider
-    when 'facebook', 'google', 'vkontakte'
+    when 'facebook', 'google', 'vkontakte', 'mailru'
       auth.info.email
     when 'twitter'
       "#{auth.info.name}@twitter.com"
