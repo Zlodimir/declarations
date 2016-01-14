7d1f8a25a5ebd600fa600ec262ebed3521d55989
Sergey Lupinos <sslupinos@gmail.com>
2016-01-14 20:31:19 +0300

    meta headers changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 
-  http_basic_authenticate_with name: 'user', password: '1dkjJsdlf_s' if Rails.env.production?
+  #http_basic_authenticate_with name: 'user', password: '1dkjJsdlf_s' if Rails.env.production?
 
   protect_from_forgery with: :exception
 
