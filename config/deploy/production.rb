4a4aedb6b28ac4b339d739a345b46115368e61c2
Sergey Lupinos <sslupinos@gmail.com>
2016-04-05 08:45:27 +0300

    whenever set up

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -28,3 +28,16 @@ namespace :deploy do
   after :publishing, :restart
   after :finishing, :cleanup
 end
+
+namespace :deploy do
+  desc "Update crontab with whenever"
+  task :update_cron do
+    on roles(:app) do
+      within current_path do
+        execute :bundle, :exec, "whenever --update-crontab #{fetch(:application)}"
+      end
+    end
+  end
+
+  after :finishing, 'deploy:update_cron'
+end
